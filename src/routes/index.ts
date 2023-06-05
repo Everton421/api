950f239a0e410c4d5296ab7776273ed44cbf4045
intersig <suporte@intersig.com>
2023-06-05 20:17:59 +0000

    first commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,33 +7,65 @@ var con = mysql.createConnection({
     user: "root",
     password: "Nileduz",
     
-    database:"wp_estoque",
-    database2:"wp_publico",
-    database3:"wp_vendas",
+    database:"clseg_estoque",
+    database2:"clseg_publico",
+    database3:"clseg_vendas",
     
     
   })  
 
 export const router = Router()
 
+ router.all('/',(req,res)=>{    
+        const client = 11
+        //const reqProd = req.body
 
-/* 
-router.get('/', (req ,res )=>{
-        con.query('Select * from cep;',(err:string, result:string)=>{
+        const reqProd = req.body
+
+     con.query(`INSERT INTO clseg_vendas.cad_orca (cliente)  VALUES(${client})`,(err: string,result:any)=>{
             if(err) throw err;
-            res.json(result) 
+            
+
+            const codOrca = result.insertId;    
+            console.log(`orçamento num: ${codOrca}`)
+
+     
+            
+         
+            var produto:any = reqProd
+        
+       for(let i =0; i <=  reqProd.length; i++){
+        if(reqProd[i] ==undefined){
+     break;
+    }
+     console.log(reqProd[i].prod)
+    let j = i+1;
+
+   con.query(`INSERT INTO clseg_vendas.pro_orca (orcamento, sequencia, produto) values(${codOrca},${j}, ${produto[i].prod})`,(err:string,response:any)=>{
+                if(err) throw err;
+            })
+            console.log("produto inserido"+reqProd[i].prod)
+     
+        }      
+
+})
+
+    });
+
+    router.get('/produtos',(req,res)=>{
+        con.query('select cp.codigo,cp.descricao,tp.PRECO from clseg_publico.cad_prod cp join clseg_publico.prod_tabprecos tp on tp.PRODUTO = cp.CODIGO where tp.TABELA = 1 LIMIT 20;',(err:string, response:any)=>{
+            if(err) throw err;
+            res.json(response)
         })
-    
-})
 
-*/
-router.get('/', (req ,res )=>{
-    con.query(
-        `select cp.codigo, cp.descricao, ps.estoque from wp_publico.cad_prod cp join wp_estoque.prod_setor ps on cp.codigo = ps.produto;`
-    ,(err:string, result:string)=>{
-        if(err) throw err;
-        res.json(result) 
+        router.get('/clientes',(req,res)=>{
+            con.query('select codigo,nome ,cpf from clseg_publico.cad_clie;',(err:string, response:any)=>{
+                if(err) throw err;
+                res.json(response)
+            })
+        })
+
+      
+
+
     })
-
-})
-
